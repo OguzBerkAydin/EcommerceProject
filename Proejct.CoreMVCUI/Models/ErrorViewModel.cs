b868c953b07e8a6c24172e5b83a63b1c2b576e62
Oguz Berk AYDIN <104473831+SosisliYumurta@users.noreply.github.com>
2024-02-16 20:39:27 +0300

    Project Layers Created

---
+++ Proejct.CoreMVCUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proejct.CoreMVCUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
